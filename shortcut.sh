fa2bfc793ab02a9d5c734398a42bffb94a372dd3
benjamin <210059635+bennvk@users.noreply.github.com>
2025-08-10 18:21:57 +0200

    Update shortcut.sh

--- shortcut.sh
+++ shortcut.sh
@@ -10,17 +10,26 @@ while true; do
         echo "Erreur : fichier introuvable dans $HOME/gpxdot/$fichier. Réessayez."
     fi
 
-    read -p "Distance entre les points (en mètres) : " distance || echo "Distance entrée invalide"
-read -p "Rediriger la sortie dans un fichier ? [O/n]" filed
+read -p "Distance entre les points (en mètres) : " distance
 
-if [[ $filed == "O" || $filed == "0" || $filed == "o" ]]; then
+case $choix_fichier in
+    [0Oo]) gpxdot_fichier
+           ;;
+    [Non]) gpxdot_sansfichier
+           ;;
+    *) echo "Choix non valide, veuillez réessayer"
+       exit 1
+       ;;
+
+gpxdot_fichier() {
     read -p "Nom du fichier de sortie : " output
     echo ""
     echo " Conversion du fichier en cours ... "
     mkdir -p ~/gpxdot/Fichiers_texte
     python3 ~/gpxdot/gpxdot.py ~/gpxdot/$fichier $distance > ~/gpxdot/Fichiers_texte/$output
     echo ""
-    echo " Fichier texte copié dans le répertoire /home/olivier/gpxdot/$output "
+    echo " Fichier copié dans le répertoire $HOME/gpxdot/$output"
+}
 else
     echo ""
     echo "Conversion du fichier en cours ..."
